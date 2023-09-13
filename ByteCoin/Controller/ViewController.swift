acde0148d524692463750e77a789c22fdf7aa56c
Amit Singh <amitsighbxr@gmail.com>
2023-09-13 15:39:44 +0800

    Final commit with rest api implementation

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , CoinManagerDelegate {
+    func didUpadtePrice(price: String, currency: String) {
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = price
+            self.currencylabel.text = currency
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+    
+    
+    var coinManager = CoinManager()
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        coinManager.delegate = self
         // Do any additional setup after loading the view.
+      //  perormRequest(with: coinManager.baseURL)
     }
-
-
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    @IBOutlet weak var currencylabel: UILabel!
+    
+   
+    
 }
-
+extension ViewController : UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return  coinManager.currencyArray.count
+        
+    }
+}
+extension ViewController : UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+      let selectedCurrency = coinManager.currencyArray[row]
+       coinManager.getCoinPrice(for: selectedCurrency)
+    }
+    
+}
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -7,13 +7,47 @@
 //
 
 import Foundation
-
+protocol  CoinManagerDelegate {
+    func didUpadtePrice(price : String ,currency : String)
+    func didFailWithError(error : Error)
+}
 struct CoinManager {
-    
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    var delegate :CoinManagerDelegate?
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC/?apikey=D176BB92-7FE9-4C61-A805-832F8D9683AF"
     let apiKey = "YOUR_API_KEY_HERE"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
+    func getCoinPrice(for currency : String) {
+        let urlString = "https://rest.coinapi.io/v1/exchangerate/BTC/\(currency)?apikey=D176BB92-7FE9-4C61-A805-832F8D9683AF"
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print("Eror :\(error!)")
+                    return
+                }
+                if let safeData = data {
+                    let bicoinPrice = self.parseJSON(safeData)
+                    let priceString = String(format: "%.1f", bicoinPrice)
+                    self.delegate?.didUpadtePrice(price: priceString, currency: currency)
+                }
+            }
+            task.resume()
+        }
+    
+    }
+    func parseJSON (_ data : Data) -> Double {
+        let decoder = JSONDecoder()
+        do {
+           let decodedData =  try decoder.decode(CoinData.self, from: data)
+            let rate = decodedData.rate
+            print(rate)
+            return rate
+        }
+        catch {
+            print(error)
+            return 0.0
+        }
+    }
     
 }
